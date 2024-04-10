6082c4097f70262be17f776cb8e8d221119c61d0
Veikkosuhonen <vesuvesuh@gmail.com>
2024-04-10 21:02:03 +0300

    Scaffold #2

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,27 @@
-import { Router } from "@solidjs/router";
-import { FileRoutes } from "@solidjs/start/router";
-import { Suspense } from "solid-js";
-import Nav from "~/components/Nav";
-import "./app.css";
+import type { Component } from 'solid-js';
 
-export default function App() {
+import logo from './logo.svg';
+import styles from './App.module.css';
+
+const App: Component = () => {
   return (
-    <Router
-      root={props => (
-        <>
-          <Nav />
-          <Suspense>{props.children}</Suspense>
-        </>
-      )}
-    >
-      <FileRoutes />
-    </Router>
+    <div class={styles.App}>
+      <header class={styles.header}>
+        <img src={logo} class={styles.logo} alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          class={styles.link}
+          href="https://github.com/solidjs/solid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Solid
+        </a>
+      </header>
+    </div>
   );
-}
+};
+
+export default App;
